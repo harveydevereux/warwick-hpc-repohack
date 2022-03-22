2700f15e756045b54be628c2dcf3f9824af2bdf9
Harvey Devereux <matwdz@node010.sulis.hpc>
2022-03-22 14:05:48 +0000

    initial conditions, move origin

---
+++ tutorial/bump.c
@@ -0,0 +1,26 @@
+#include "saint-venant.h"
+/*
+ * find s-v params here http://basilisk.fr/src/saint-venant.h
+ */
+
+/*
+ * An init event runs once at time t=0 an sets the depth field h[] - note the nice
+ * foreach syntax basilisk provides http://basilisk.fr/Basilisk%20C#iterators
+ */
+event init (t = 0){
+	foreach()
+		// guassian
+		h[] = 0.1 + 1.*exp(-200.*(x*x+y*y));
+}
+/*
+ * event is a function, i=10 tells basilisk to do 10 time steps
+ */
+event end (i=10){
+	printf ("i=%d t = %g\n",i,t);
+}
+
+int main(){
+	// run() simulates by defualt square box of length  1, reflective bcs, origin at lower left
+	origin (-0.5, -0.5); // specify origin (centre of box now)
+	run();
+}
